10cecf8053454495d8638464d5fe43b28071ae44
bkjohn2018 <38437474+bkjohn2018@users.noreply.github.com>
2025-01-25 17:11:05 -0700

    Add project files.

---
+++ CapPredict/CapPredict.py
@@ -0,0 +1,10 @@
+
+import sys
+import numpy as np
+import pandas as pd
+
+print(f"Python executable: {sys.executable}")
+print(f"Python version: {sys.version}")
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+# This is a test change to push to GitHub
